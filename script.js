ef3fe6d2a5241a7ee2026b8a74ae91ecd25d1f92
SATHEESH PERIYASAMY <101659247+SatheeshPeriyasamy@users.noreply.github.com>
2024-02-14 12:13:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getDatabase, ref,set,get,child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+let searchForm = document.querySelector('.search-form');
 
-  
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+}
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDlLRXqFegDE8Bv2npjCQEVVBXEHo7zXU0",
-    authDomain: "travelsite-fb1a7.firebaseapp.com",
-    projectId: "travelsite-fb1a7",
-    storageBucket: "travelsite-fb1a7.appspot.com",
-    messagingSenderId: "79839121496",
-    appId: "1:79839121496:web:aafae4990dcdab844d3a2d"
-  };
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+window.onload=()=>{
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-//   get ref to db
-const db = getDatabase(app);
-
-document.getElementById("submit").addEventListener('click', function(e){
-    e.preventDefault();
-    set(ref(db, 'user/' + document.getElementById("username").value),
-    {
-
-      Username: document.getElementById("username").value,
-      Email: document.getElementById("email").value,
-      PhoneNumber: document.getElementById("phone").value
-
-    });
-      alert("Logged in Successfully!!!");
-   })
